cdce14e34794db44040fd7239f3d23fe99a87460
Adnan <adnankhan07666@gmail.com>
2022-08-16 08:08:31 +0530

    inotebook is completed

---
+++ src/componenets/About.js
@@ -0,0 +1,11 @@
+// import React, { useContext, useEffect } from 'react'
+// import NoteContext from '../context/notes/NoteContext'
+
+const About = () => {
+
+    return (
+        <div>This is About Page</div>
+    )
+}
+
+export default About;
